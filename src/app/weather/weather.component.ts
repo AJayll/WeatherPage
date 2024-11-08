02b07d4a50e5d32b6fd80ea8e21422492f598761
Alan Llewellyn <alanllewellyn1@gmail.com>
2024-11-08 12:46:01 -0500

    Added a bunch of css styles from the angular tutorial, implemented weather component with non functional search bar + button, banner issue TBD

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.css'
 })
