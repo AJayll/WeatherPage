fc9d468cc9988f4ed12351509555361bce0723f4
Alan Llewellyn <alanllewellyn1@gmail.com>
2024-11-08 14:43:35 -0500

    Generated component that will eventually hold and display results from the API lookup

---
+++ src/app/weather-location/weather-location.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-weather-location',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather-location.component.html',
+  styleUrl: './weather-location.component.css'
+})
+export class WeatherLocationComponent {
+
+}
--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {WeatherLocationComponent} from '../weather-location/weather-location.component';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
+  imports: [WeatherLocationComponent],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.css'
 })
