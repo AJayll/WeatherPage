5ea80ff1a301ce10979ec3044c9dafdc908f6aab
Alan Llewellyn <alanllewellyn1@gmail.com>
2024-11-10 13:25:14 -0500

    Added tutorial for each loop of multiple cities, unlikely to stay for final product

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,22 +1,52 @@
 import { Component } from '@angular/core';
 import {WeatherLocationComponent} from '../weather-location/weather-location.component';
+import { CommonModule } from '@angular/common';
 import {WeatherLocation} from '../weather-location';
 
 @Component({
   selector: 'app-weather',
   standalone: true,
-  imports: [WeatherLocationComponent],
+  imports: [CommonModule, WeatherLocationComponent],
   templateUrl: './weather.component.html',
   styleUrl: './weather.component.css'
 })
 export class WeatherComponent {
-  weatherLocation: WeatherLocation = {
-    id: 9999,
+  //Temp data, eventually pull cities (city?) from API with input being from the search bar
+  weatherLocationList: WeatherLocation[] = [{
+    id: 0,
     temperature: 75,
     city: 'Tampa',
     state: 'FL',
     forecast: "Sunny",
     details: "Humidity 84%, Wind: 8mph NE",
     celsius: false,
-  };
+  },
+  {
+    id: 1,
+    temperature: 78,
+    city: 'Weston',
+    state: 'FL',
+    forecast: "Cloudy",
+    details: "Humidity 86%, Wind: 4mph NE",
+    celsius: false,
+  },
+  {
+    id: 2,
+    temperature: 78,
+    city: 'Davie',
+    state: 'FL',
+    forecast: "Cloudy",
+    details: "Humidity 86%, Wind: 4mph NE",
+    celsius: false,
+  },
+  {
+    id: 3,
+    temperature: 72,
+    city: 'Naples',
+    state: 'FL',
+    forecast: "Rainy",
+    details: "Humidity 88%, Wind: 6mph NW",
+    celsius: false,
+  },
+];
 }
