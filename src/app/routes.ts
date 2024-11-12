70ec437ccdcb38cf5c8b6fe91ab3521431e4fbd7
Alan Llewellyn <alanllewellyn1@gmail.com>
2024-11-12 00:21:11 -0500

    Got rid of extraneous detail page from angular tutorial

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,17 +1,11 @@
 import { Routes } from '@angular/router';
 import { WeatherComponent } from './weather/weather.component';
-import { DetailsComponent } from './details/details.component';
 
 const routeConfig: Routes = [
     {
       path: '',
       component: WeatherComponent,
       title: 'City page',
-    },
-    {
-      path: 'details/:id',
-      component: DetailsComponent,
-      title: 'Weather details',
     },
   ];
   export default routeConfig;
